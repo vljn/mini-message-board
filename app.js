a55047c473b4daa791a9fbba1fe1cc20e0ffe7aa
vljn <121915880+vljn@users.noreply.github.com>
2024-07-22 04:27:02 +0200

    Add the .env file with PORT number

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const router = require('./routers/index');
+require('dotenv').config();
 
 const app = express();
 
@@ -11,5 +12,5 @@ app.set('view engine', 'ejs');
 
 app.use(router);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Server listening on port ' + PORT));
